6fead78ee9196bd55c7a34be54087f79d4087e0f
X <lesniak721@gmail.com>
2022-04-03 18:02:24 +0200

    dzialajace przyciski dzwiekow

--- app/src/main/java/com/example/melodies/MainActivity.java
+++ app/src/main/java/com/example/melodies/MainActivity.java
@@ -14,13 +14,17 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int sound_Guitar1 = R.raw.guitar1;
     public static final int sound_Guitar2 = R.raw.guitar2;
-    public static final int sound_Bass = R.raw.bass;
-    public static final int sound_Drums = R.raw.drums;
+    public static final int sound_Bass1 = R.raw.bass1;
+    public static final int sound_Bass2 = R.raw.bass2;
+    public static final int sound_Drums1 = R.raw.drums1;
+    public static final int sound_Drums2 = R.raw.drums2;
 
     Button Guitar1;
     Button Guitar2;
-    Button Bass;
-    Button Drums;
+    Button Bass1;
+    Button Bass2;
+    Button Drums1;
+    Button Drums2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         Guitar1 = (Button) findViewById(R.id.button2);
         Guitar2 = (Button) findViewById(R.id.button4);
-        Bass = (Button) findViewById(R.id.button5);
-        Drums = (Button) findViewById(R.id.button3);
+        Bass1 = (Button) findViewById(R.id.button5);
+        Bass2 = (Button) findViewById(R.id.button7);
+        Drums1 = (Button) findViewById(R.id.button3);
+        Drums2 = (Button) findViewById(R.id.button6);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
         {
@@ -56,9 +62,25 @@ public class MainActivity extends AppCompatActivity {
         {
             sound = sound_Guitar1;
         }
-        else if(Drums.getId() == v.getId())
+        else if(Guitar2.getId() == v.getId())
         {
-            sound = sound_Drums;
+            sound = sound_Guitar2;
+        }
+        else if(Bass1.getId() == v.getId())
+        {
+            sound = sound_Bass1;
+        }
+        else if(Bass2.getId() == v.getId())
+        {
+            sound = sound_Bass2;
+        }
+        else if(Drums1.getId() == v.getId())
+        {
+            sound = sound_Drums1;
+        }
+        else if(Drums2.getId() == v.getId())
+        {
+            sound = sound_Drums2;
         }
 
         playSound(this, sound);
